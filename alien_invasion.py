204a7fafabc01d7f837f10405700db1a95ed50a8
Greg Schuman <gcschuman@hotmail.com>
2021-02-22 09:20:12 -0800

    Updated the main game engine to include the settings importation and its settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,10 +2,16 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+
     pygame.display.set_caption("Alien Invasion")
 
     # Set the background color
@@ -18,9 +24,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
 
         # Redraw the screen during each pass through the loop
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
